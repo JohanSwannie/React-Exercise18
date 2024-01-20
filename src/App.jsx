9653bf377ad13086a964bd269a2c710436f68263
Johan Swan <jswannieman@gmail.com>
2024-01-20 15:56:48 +1300

    Update App JSX Component

--- src/App.jsx
+++ src/App.jsx
@@ -45,7 +45,7 @@ function App() {
     <TodoProvider
       value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}
     >
-      <div className="bg-[#171800] min-h-screen py-8">
+      <div className="bg-[#171800] min-h-screen">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
             Todos List
