4f4103c26194a23f52220c3624a79f93ae77c658
Johan Swan <jswannieman@gmail.com>
2024-01-21 11:50:49 +1300

    Update TodoItem JSX Component

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -31,7 +31,7 @@ function TodoItem({ todo }) {
       <input
         type="text"
         className={`border outline-none w-full bg-transparent rounded-lg ${
-          isTodoEditable ? "border-black/10 px-2" : "border-transparent"
+          isTodoEditable ? "border-white px-2 py-1" : "border-transparent"
         }`}
         value={todoMsg}
         onChange={(e) => setTodoMsg(e.target.value)}
