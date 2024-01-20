191de99f5779131f9113f6405ca2c784b6243b7b
Johan Swan <jswannieman@gmail.com>
2024-01-20 18:42:24 +1300

    Update TodoItem JSX Component

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -18,7 +18,7 @@ function TodoItem({ todo }) {
 
   return (
     <div
-      className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${
+      className={`flex border border-black/10 rounded-lg px-2 py-1 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${
         todo.completed ? "bg-[#70af55]" : "bg-[#84caa4]"
       }`}
     >
